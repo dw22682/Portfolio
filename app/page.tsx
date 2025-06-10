24952b1c8df15a2f3d38b186ec86709d5b78090b
dw22682 <123548484+dw22682@users.noreply.github.com>
2025-06-10 19:12:14 +0100

    Update page.tsx
    just changng the text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <div>Next.js on GitHub Pages</div>
+      <div>Hi this is Josh Chatten, This is my portfolio website</div>
+      <div>I am using Next.js and GitHub Pages to run it</div>
+      <div>It is currently in pre-development</div>
     </main>
   );
 }
