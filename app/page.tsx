95d4660a1f9d4e0e76643265931877e06f672ffd
Josh <dw22682@bristol.ac.uk>
2025-06-12 22:39:28 +0100

    v0.0.1 time for development :)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <main>
+      <div>Josh Chatten Portfolio v0.0.1</div>
       <div>Hi this is Josh Chatten, This is my portfolio website</div>
       <div>I am using Next.js and GitHub Pages to run it</div>
       <div>It is currently in pre-development</div>
