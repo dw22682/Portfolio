1f0f151f4d9292a4b9cf93c12d927c3999aec0c4
Josh <dw22682@bristol.ac.uk>
2025-06-17 21:53:56 +0100

    good start

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Image from 'next/image'
+
+const HeroSection = () => {
+  let x = 300;
+  return (
+    <section>
+      <div className='grid grid-cols-1 sm:grid-cols-12'>
+        <div className='col-span-7 place-self-center text-center sm:text-left'>
+          <h1 className='lg:text-6xl text-4xl sm:text-5xl mb-4 font-extrabold px-10 sm:px-2'>
+            Josh Chatten Portfolio
+          </h1>
+          <p className='text-gray-100 mb-6 mr-6 text-lg lg:text-xl'>
+            Hi, I'm Josh. Welcome to my portfolio website. Here you can find projects that i have worked on along with relevant information, skills and links.
+          </p>
+          <div>
+            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-[#d2d2d2] text-black hover:bg-white'>
+              My Github
+            </button>
+            <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-[#313131] border border-white text-white mt-6'>
+              Download CV
+            </button>
+          </div>
+        </div>
+        <div className='col-span-5 place-self-center mt-4 lg:mt-0 ml-8'>
+          <div className='rounded-full bg-[#181818] w-[350px] h-[350px] sm:h-[270px] sm:w-[270px] lg:h-[450px] lg:w-[450px] relative'>
+            <Image
+              src="/images/fractal.jpg"
+              alt='profile image'
+              className='place-self-center h-[300px] w-[300px] sm:w-[200px] sm:h-[200px] absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+              width={x}
+              height={x}
+              />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HeroSection
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Portfolio</h1>
+    <main className="flex min-h-screen flex-col bg-[#131313]">
+      <div className="container mx-auto px-10 py-4">
+        <HeroSection />
+      </div>
     </main>
   );
 }
