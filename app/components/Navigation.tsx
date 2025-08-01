e77b555273ae94740d20a2974122852d6a280884
Josh <dw22682@bristol.ac.uk>
2025-08-01 17:38:05 +0100

    added mobile menu overlay and improved header

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,6 +1,9 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import NavLink from './NavLink'
+import NavOverlay from './NavOverlay'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 import { title } from 'process'
 import { link } from 'fs';
 
@@ -20,12 +23,28 @@ const navLinks = [
 ];
 
 const Navigation = () => {
+  const [barOpen, setBarOpen] = useState(false);
+
   return (
-    <nav>
-        <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
-            <Link href={"/"} className='text-2xl md:text-5xl text-white font-semibold'>
-                LOGO
+    <nav className='fixed top-0 left-0 right-0 z-10 hover:bg-[#131313]'>
+      <div className={barOpen ? 'bg-[#131313]' : ''}>
+        <div className='flex flex-wrap items-center justify-between mx-auto p-4'>
+            <Link href={"/"} className='text-2xl md:text-4xl text-white font-semibold'>
+                HOME
             </Link>
+            <div className='mobile-menu block md:hidden'>
+              {
+                !barOpen ? (
+                  <button onClick={() => setBarOpen(true)} className='flex items-center px-3 py-2 border rounded border-amber-100 text-amber-50 hover:text-white hover:border-white'>
+                    <Bars3Icon className='h5- w-5' />
+                  </button>
+                 ) : (
+                  <button onClick={() => setBarOpen(false)} className='flex items-center px-3 py-2 border rounded border-amber-100 text-amber-50 hover:text-white hover:border-white'>
+                    <XMarkIcon className='h5- w-5' />
+                  </button>
+                 )
+              }
+            </div>
             <div className="menu hidden md:block md:w-auto" id="navigation">
               <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
                 {
@@ -39,6 +58,8 @@ const Navigation = () => {
               </ul>
             </div>
         </div>
+      </div>
+      {barOpen ? <NavOverlay navLinks={navLinks}/> : null}
     </nav>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-gradient-to-tr from-[#131313] via-[#141414] to-yellow-600">
       <Navigation />
-      <div className="container mx-auto px-10 py-4">
+      <div className="container mx-auto px-10 py-4 mt-24">
         <HeroSection />
       </div>
     </main>
