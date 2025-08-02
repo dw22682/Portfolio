a75a6c7f174017dc30d10bcb17c4390f9476f3a9
Josh <dw22682@bristol.ac.uk>
2025-08-02 23:24:46 +0100

    changed hover colours on header

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -29,17 +29,17 @@ const Navigation = () => {
     <nav className='fixed top-0 left-0 right-0 z-10 hover:bg-[#131313]'>
       <div className={barOpen ? 'bg-[#131313]' : ''}>
         <div className='flex flex-wrap items-center justify-between mx-auto p-4'>
-            <Link href={"/"} className='text-2xl md:text-4xl text-white font-semibold'>
+            <Link href={"/"} className='text-2xl md:text-4xl text-white font-semibold hover:text-teal-200'>
                 HOME
             </Link>
             <div className='mobile-menu block md:hidden'>
               {
                 !barOpen ? (
-                  <button onClick={() => setBarOpen(true)} className='flex items-center px-3 py-2 border rounded border-amber-100 text-amber-50 hover:text-white hover:border-white'>
+                  <button onClick={() => setBarOpen(true)} className='flex items-center px-3 py-2 border rounded border-amber-100 text-amber-50 hover:text-teal-200 hover:border-teal-200'>
                     <Bars3Icon className='h5- w-5' />
                   </button>
                  ) : (
-                  <button onClick={() => setBarOpen(false)} className='flex items-center px-3 py-2 border rounded border-amber-100 text-amber-50 hover:text-white hover:border-white'>
+                  <button onClick={() => setBarOpen(false)} className='flex items-center px-3 py-2 border rounded border-amber-100 text-amber-50 hover:text-teal-200 hover:border-teal-200'>
                     <XMarkIcon className='h5- w-5' />
                   </button>
                  )
