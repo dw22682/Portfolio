dbd55432e8ada3056b28afc37f1ca7bcdcb09ef9
Josh <dw22682@bristol.ac.uk>
2025-08-04 02:16:28 +0100

    project section tmp done

---
+++ app/components/ProjectSection.tsx
@@ -0,0 +1,83 @@
+"use client"
+import React, { useState } from 'react'
+import ProjectCard from './ProjectCard'
+import ProjectTag from './ProjectTag'
+
+const projectData = [
+    {
+        id: 1,
+        title: "Shifting Sands",
+        description: "Best Video Game of ALL TIME!",
+        image: "/images/projects/seal_team7_group_photo.jpg",
+        tag: ["All", "Game"],
+        gitUrl: "https://github.com/lm22433/2024-SealTeam7",
+        previewUrl: "/"
+    },
+    {
+        id: 2,
+        title: "Project 2",
+        description: "Description of Project 2",
+        image: "/images/projects/seal_team7_group_photo.jpg",
+        tag: ["All", "Web"],
+        gitUrl: "/",
+        previewUrl: "/"
+    },
+    {
+        id: 3,
+        title: "Project 3",
+        description: "Description of Project 3",
+        image: "/images/projects/seal_team7_group_photo.jpg",
+        tag: ["All", "Web"],
+        gitUrl: "/",
+        previewUrl: "/"
+    }
+]
+
+const ProjectSection = () => {
+    const [tag, setTag] = useState("All")
+    const handleTagChange = (newTag: string) => {
+        setTag(newTag);
+    }
+    const filteredData = projectData.filter((project) => {
+        return project.tag.includes(tag);
+    })
+
+  return (
+    <div>
+        <h2 className='text-center text-4xl font-bold text-amber-100 mb-4 mt-4'>
+            My Projects
+        </h2>
+        <div className='text-amber-50 flex flex-row justify-center items-center gap-2 py-6'>
+            <ProjectTag
+               onClick={handleTagChange}
+                name='All'
+                isSelected={tag === "All"}
+            />
+            <ProjectTag
+               onClick={handleTagChange}
+                name='Game'
+                isSelected={tag === "Game"}
+            /> 
+            <ProjectTag
+               onClick={handleTagChange}
+                name='Web'
+                isSelected={tag === "Web"}
+            /> 
+        </div>
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-6 lg:gap-12'>
+            {
+                filteredData.map((project) =>(
+                <ProjectCard key={project.id}
+                             title={project.title}
+                             description={project.description}
+                             imgURL={project.image}
+                             gitURL={project.gitUrl}
+                             previewURL={project.previewUrl} />
+                ))
+            }
+        </div>
+    </div>
+  )
+}
+
+export default ProjectSection
