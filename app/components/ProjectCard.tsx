24b91c1cda57c6dd03d204981ba621eaac4f86e5
Josh <dw22682@bristol.ac.uk>
2025-10-23 12:22:46 +0100

    improved mugshot and touchscreen hover simulation?

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -17,12 +17,12 @@ const ProjectCard = ({ imgURL, title, description, gitURL, previewURL }: typeDef
             className='h-72 lg:h-92 rounded-t-xl relative group'
             style={{ background: `url(${imgURL})`, backgroundSize:"cover"}}
         >
-            <div className='overlay flex items-center justify-center absolute top-0 left-0 w-full h-full group-hover:flex group-hover:bg-[#141414]/80'>
-                <Link className='hidden group-hover:block mr-2 h-14 w-14 border-2 relative rounded-full group-hover:border-amber-100 hover:border-white group/link' href={gitURL}>
-                    <CodeBracketIcon className='h-10 w-10 group-hover:text-amber-100 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white' />
+            <div className='overlay flex items-center justify-center absolute top-0 left-0 w-full h-full group-hover:flex group-hover:bg-[#141414]/80 group-focus:flex group-focus:bg-[#141414]'>
+                <Link className='hidden group-hover:block group-focus:block mr-2 h-14 w-14 border-2 relative rounded-full group-hover:border-amber-100 hover:border-white group-focus:border-amber-100 focus:border-white group/link' href={gitURL}>
+                    <CodeBracketIcon className='h-10 w-10 group-hover:text-amber-100 group-focus:text-amber-100 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white group-focus/link:text-white' />
                 </Link>
-                <Link className='hidden group-hover:block ml-2 h-14 w-14 border-2 relative rounded-full group-hover:border-amber-100 hover:border-white group/link' href={previewURL}>
-                    <EyeIcon className='h-10 w-10 group-hover:text-amber-100 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white' />
+                <Link className='hidden group-hover:block group-focus:block ml-2 h-14 w-14 border-2 relative rounded-full group-hover:border-amber-100 hover:border-white group-focus:border-amber-100 focus:border-white group/link' href={previewURL}>
+                    <EyeIcon className='h-10 w-10 group-hover:text-amber-100 group-focus:text-amber-100 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white group-focus/link:text-white' />
                 </Link>            
             </div>
         </div>
